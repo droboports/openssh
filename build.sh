4da982eb074fed911f2cbfa6eabaa4c2ea0442c5
Ricardo Padilha <ricardospadilha@gmail.com>
2014-09-24 18:01:47 +0200

    First version

---
+++ build.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+### bash best practices ###
+# exit on error code
+set -o errexit
+# exit on unset variable
+set -o nounset
+# return error of last failed command in pipe
+set -o pipefail
+# expand aliases
+shopt -s expand_aliases
+# print trace
+set -o xtrace
+
+### logfile ###
+timestamp="$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="logfile_${timestamp}.txt"
+echo "${0} ${@}" > "${logfile}"
+# save stdout to logfile
+exec 1> >(tee -a "${logfile}")
+# redirect errors to stdout
+exec 2> >(tee -a "${logfile}" >&2)
+
+### environment variables ###
+source crosscompile.sh
+export NAME="openssh"
+export DEST="/mnt/DroboFS/Shares/DroboApps/${NAME}"
+export DEPS="/mnt/DroboFS/Shares/DroboApps/${NAME}deps"
+export CFLAGS="${CFLAGS:-} -Os -fPIC"
+export CXXFLAGS="${CXXFLAGS:-} ${CFLAGS}"
+export CPPFLAGS="-I${DEPS}/include"
+export LDFLAGS="${LDFLAGS:-} -Wl,-rpath,${DEST}/lib -L${DEST}/lib"
+alias make="make -j8 V=1 VERBOSE=1"
+
+# $1: file
+# $2: url
+# $3: folder
+_download_tgz() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  [[ ! -d "target/${3}" ]] && tar -zxvf "download/${1}" -C target
+}
+
+### ZLIB ###
+_build_zlib() {
+local VERSION="1.2.8"
+local FOLDER="zlib-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://zlib.net/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --prefix="${DEPS}" --libdir="${DEST}/lib"
+make
+make install
+rm -v "${DEST}/lib"/*.a
+popd
+}
+
+### OPENSSL ###
+_build_openssl() {
+local OPENSSL_VERSION="1.0.1i"
+local OPENSSL_FOLDER="openssl-${OPENSSL_VERSION}"
+local OPENSSL_FILE="${OPENSSL_FOLDER}.tar.gz"
+local OPENSSL_URL="http://www.openssl.org/source/${OPENSSL_FILE}"
+
+_download_tgz "${OPENSSL_FILE}" "${OPENSSL_URL}" "${OPENSSL_FOLDER}"
+pushd target/"${OPENSSL_FOLDER}"
+./Configure --prefix="${DEPS}" \
+  --openssldir="${DEST}/etc/ssl" \
+  --with-zlib-include="${DEPS}/include" \
+  --with-zlib-lib="${DEPS}/lib" \
+  shared zlib-dynamic threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
+sed -i -e "s/-O3//g" Makefile
+make -j1
+make install_sw
+mkdir -p "${DEST}"/libexec
+cp -avR "${DEPS}/bin/openssl" "${DEST}/libexec/"
+cp -avR "${DEPS}/lib"/* "${DEST}/lib/"
+rm -fvr "${DEPS}/lib"
+rm -fv "${DEST}/lib"/*.a
+sed -i -e "s|^exec_prefix=.*|exec_prefix=${DEST}|g" "${DEST}"/lib/pkgconfig/openssl.pc
+popd
+}
+
+### OPENSSH ###
+_build_openssh() {
+local VERSION="6.6p1"
+local FOLDER="openssh-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://mirror.switch.ch/ftp/pub/OpenBSD/OpenSSH/portable/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+sed -i -e "s/sshd\.pid/pid.txt/" pathnames.h
+./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --with-zlib="${DEPS}" --disable-strip --with-ssl-dir="${DEPS}" --with-pid-dir=/tmp/DroboApps/openssh --with-sandbox=rlimit --with-privsep-path="${DEST}/var/empty" --with-privsep-user=sshd select_works_with_rlimit=yes
+make
+make install-nokeys
+popd
+}
+
+### BUILD ###
+_build() {
+  _build_zlib
+  _build_openssl
+  _build_openssh
+  _package
+}
+
+_create_tgz() {
+  local appname="$(basename ${PWD})"
+  local appfile="${PWD}/${appname}.tgz"
+
+  if [[ -f "${appfile}" ]]; then
+    rm -v "${appfile}"
+  fi
+
+  pushd "${DEST}"
+  tar --verbose --create --numeric-owner --owner=0 --group=0 --gzip --file "${appfile}" *
+  popd
+}
+
+_package() {
+  mv -v "${DEST}/etc/ssh_config"{,.default} || true
+  mv -v "${DEST}/etc/sshd_config"{,.default} || true
+
+  cp -v -faR src/dest/* "${DEST}"/
+  chmod -R g-w "${DEST}/var/empty"
+  find "${DEST}" -name "._*" -print -delete
+  _create_tgz
+}
+
+_clean() {
+  rm -v -fr "${DEPS}"
+  rm -v -fr "${DEST}"
+  rm -v -fr target/*
+}
+
+_dist_clean() {
+  _clean
+  rm -v -f logfile*
+  rm -v -fr download/*
+}
+
+case "${1:-}" in
+  clean)     _clean ;;
+  distclean) _dist_clean ;;
+  package)   _package ;;
+  "")        _build ;;
+  *)         _build_${1} ;;
+esac
---
+++ src/dest/service.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env sh
+#
+# ProFTPd service
+
+# import DroboApps framework functions
+. /etc/service.subr
+
+# DroboApp framework version
+framework_version="2.0"
+
+# app description
+name="openssh"
+version="6.6"
+description="SSH server"
+
+# framework-mandated variables
+pidfile="/tmp/DroboApps/${name}/pid.txt"
+logfile="/tmp/DroboApps/${name}/log.txt"
+statusfile="/tmp/DroboApps/${name}/status.txt"
+errorfile="/tmp/DroboApps/${name}/error.txt"
+
+# app-specific variables
+prog_dir="$(dirname $(realpath ${0}))"
+rsakey="${prog_dir}/etc/ssh_host_rsa_key"
+dsakey="${prog_dir}/etc/ssh_host_dsa_key"
+ecdsakey="${prog_dir}/etc/ssh_host_ecdsa_key"
+daemon="${prog_dir}/sbin/sshd"
+
+# script hardening
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o pipefail # propagate last error code on pipe
+
+# ensure log folder exists
+logfolder="$(dirname ${logfile})"
+[[ ! -d "${logfolder}" ]] && mkdir -p "${logfolder}"
+
+# redirect all output to logfile
+exec 3>&1 1>> "${logfile}" 2>&1
+
+# log current date, time, and invocation parameters
+echo $(date +"%Y-%m-%d %H-%M-%S"): ${0} ${@}
+
+# enable script tracing
+set -o xtrace
+
+_fix_permissions() {
+  chmod a+rw /dev/null /dev/full /dev/random /dev/urandom /dev/tty /dev/ptmx /dev/zero /dev/crypto
+  touch /var/log/lastlog
+  [[ ! -f /etc/login.defs ]] && touch /etc/login.defs
+  chmod 4711 "${prog_dir}/libexec/ssh-keysign"
+  chmod -R go-w "${homedir}"
+}
+
+_create_user() {
+  id -g sshd || addgroup -g 50 sshd
+  id -u sshd || adduser -S -H -h "${homedir}" -D -s /bin/false -G sshd -u 50 sshd
+}
+
+_create_config() {
+  local dst
+  for src in "${prog_dir}/etc"/*.default; do
+    dst="${prog_dir}/etc/$(basename ${src} .default)"
+    [[ ! -f "${dst}" ]] && cp -v "${src}" "${dst}"
+  done
+}
+
+_create_keys() {
+  [[ ! -f "${rsakey}" ]] && "${prog_dir}/bin/ssh-keygen" -t rsa -f "${rsakey}" -N ""
+  [[ ! -f "${dsakey}" ]] && "${prog_dir}/bin/ssh-keygen" -t dsa -f "${dsakey}" -N ""
+  [[ ! -f "${ecdsakey}" ]] && "${prog_dir}/bin/ssh-keygen" -t ecdsa -f "${ecdsakey}" -N "" 
+}
+
+start() {
+  _fix_permissions
+  _create_user
+  _create_config
+  _create_keys
+  "${daemon}"
+}
+
+_service_start() {
+  # disable error code and unset variable checks
+  set +e
+  set +u
+  # /etc/service.subr uses DROBOAPPS without setting it first
+  DROBOAPPS=""
+  # 
+  start_service
+  set -u
+  set -e
+}
+
+_service_stop() {
+  /sbin/start-stop-daemon -K -x "${daemon}" -p "${pidfile}" -v || echo "${name} is not running" >&3
+}
+
+_service_restart() {
+  service_stop
+  sleep 3
+  service_start
+}
+
+_service_status() {
+  status >&3
+}
+
+_service_help() {
+  echo "Usage: $0 [start|stop|restart|status]" >&3
+  set +e
+  exit 1
+}
+
+case "${1:-}" in
+  start|stop|restart|status) _service_${1} ;;
+  *) _service_help ;;
+esac
