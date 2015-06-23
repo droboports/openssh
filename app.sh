f9487448bfffafd9aa364db800d3ce34ee00b554
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-23 13:42:03 -0700

    Added service.subr, updated openssl with noexecstack.

--- app.sh
+++ app.sh
@@ -24,12 +24,10 @@ local URL="http://www.openssl.org/source/${FILE}"
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 cp -vf "src/${FOLDER}-parallel-build.patch" "target/${FOLDER}/"
 pushd "target/${FOLDER}"
-patch -p1 < "${FOLDER}-parallel-build.patch"
-./Configure --prefix="${DEPS}" \
-  --openssldir="${DEST}/etc/ssl" \
-  --with-zlib-include="${DEPS}/include" \
-  --with-zlib-lib="${DEPS}/lib" \
-  shared zlib-dynamic threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
+patch -p1 -i "${FOLDER}-parallel-build.patch"
+./Configure --prefix="${DEPS}" --openssldir="${DEST}/etc/ssl" \
+  zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/lib" \
+  shared threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS} -Wa,--noexecstack -Wl,-z,noexecstack
 sed -i -e "s/-O3//g" Makefile
 make
 make install_sw
--- src/dest/service.sh
+++ src/dest/service.sh
@@ -5,144 +5,60 @@
 # import DroboApps framework functions
 . /etc/service.subr
 
-### app-specific section
-
-# DroboApp framework version
-framework_version="2.0"
-
-# app description
+framework_version="2.1"
 name="openssh"
 version="6.8"
 description="SSH server"
+depends=""
+webui=""
 
-# framework-mandated variables
-pidfile="/tmp/DroboApps/${name}/pid.txt"
-logfile="/tmp/DroboApps/${name}/log.txt"
-statusfile="/tmp/DroboApps/${name}/status.txt"
-errorfile="/tmp/DroboApps/${name}/error.txt"
-
-# app-specific variables
-prog_dir="$(dirname $(realpath ${0}))"
+prog_dir="$(dirname "$(realpath "${0}")")"
 daemon="${prog_dir}/sbin/sshd"
 homedir="${prog_dir}/var/empty"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
+statusfile="${tmp_dir}/status.txt"
+errorfile="${tmp_dir}/error.txt"
+
+# backwards compatibility
+if [ -z "${FRAMEWORK_VERSION:-}" ]; then
+  . "${prog_dir}/libexec/service.subr"
+fi
 
 # fixes device permisions to enable non-root logins
 _fix_permissions() {
   local devices="/dev/null /dev/full /dev/random /dev/urandom /dev/tty /dev/ptmx /dev/zero /dev/crypto"
   for device in ${devices}; do
-    if [[ -c "${device}" ]]; then
-      if [[ "$(stat -c %a ${device})" -ne "666" ]]; then
+    if [ -c "${device}" ]; then
+      if [ "$(stat -c %a ${device})" -ne "666" ]; then
         chmod a+rw "${device}"
       fi
     fi
   done
-  if [[ ! -f /var/log/lastlog ]]; then touch /var/log/lastlog; fi
-  if [[ ! -f /etc/login.defs ]]; then touch /etc/login.defs; fi
+  if [ ! -f "/var/log/lastlog" ]; then touch "/var/log/lastlog"; fi
+  if [ ! -f "/etc/login.defs" ]; then touch "/etc/login.defs"; fi
   chmod 4711 "${prog_dir}/libexec/ssh-keysign"
 }
 
-# _is_running
-# returns: 0 if app is running, 1 if not running or pidfile does not exist.
-_is_running() {
-  /sbin/start-stop-daemon -K -t -x "${daemon}" -p "${pidfile}" -q
-}
-
-# _is_stopped
-# returns: 0 if stopped, 1 if running.
-_is_stopped() {
-  if _is_running; then
-    return 1;
-  fi
-  return 0;
-}
-
 start() {
-  set -u # exit on unset variable
-  set -e # exit on uncaught error code
-  set -x # enable script trace
   _fix_permissions
   "${daemon}"
 }
 
-# override /etc/service.subrc
-stop_service() {
-  if _is_stopped; then
-    echo ${name} is not running >&3
-    if [[ "${1:-}" == "-f" ]]; then
-      return 0
-    else
-      return 1
-    fi
-  fi
-  /sbin/start-stop-daemon -K -x "${daemon}" -p "${pidfile}" -v
+restart() {
+  start-stop-daemon -K -s HUP -x "${daemon}" -p "${pidfile}" -v
 }
 
-reload_service() {
-  /sbin/start-stop-daemon -K -s HUP -x "${daemon}" -p "${pidfile}" -v
-}
-
-### common section
-
-# script hardening
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+STDOUT=">&3"
+STDERR=">&4"
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
 set -o errexit  # exit on uncaught error code
 set -o nounset  # exit on unset variable
 set -o pipefail # propagate last error code on pipe
+set -o xtrace   # enable script tracing
 
-# ensure log folder exists
-if ! grep -q ^tmpfs /proc/mounts; then mount -t tmpfs tmpfs /tmp; fi
-logfolder="$(dirname ${logfile})"
-if [[ ! -d "${logfolder}" ]]; then mkdir -p "${logfolder}"; fi
-
-# redirect all output to logfile
-exec 3>&1 1>> "${logfile}" 2>&1
-
-# log current date, time, and invocation parameters
-echo $(date +"%Y-%m-%d %H-%M-%S"): ${0} ${@}
-
-_service_start() {
-  if _is_running; then
-    echo ${name} is already running >&3
-    return 1
-  fi
-  set +x # disable script trace
-  set +e # disable error code check
-  set +u # disable unset variable check
-  start_service
-}
-
-_service_stop() {
-  stop_service
-}
-
-_service_waitstop() {
-  stop_service -f
-  while ! _is_stopped; do
-    sleep 1
-  done
-}
-
-_service_reload() {
-  reload_service
-}
-
-_service_restart() {
-  _service_waitstop
-  _service_start
-}
-
-_service_status() {
-  status >&3
-}
-
-_service_help() {
-  echo "Usage: $0 [start|stop|waitstop|reload|restart|status]" >&3
-  exit 1
-}
-
-# enable script tracing
-set -o xtrace
-
-case "${1:-}" in
-  start|stop|waitstop|reload|restart|status) _service_${1} ;;
-  *) _service_help ;;
-esac
+main "${@}"
